d989fb674b65babf3286a23f73b3a2bb9ac84750
polamjag <s@polamjag.info>
2015-02-03 23:22:09 +0900

    single thread

--- lib/midicat/cli.rb
+++ lib/midicat/cli.rb
@@ -3,34 +3,33 @@ module Midicat
     attr_reader :devices
 
     def initialize(devices)
-      @threads = []
       @devices = []
       @ni = Nibbler.new
 
       devices.each_with_index do |d, i|
         puts d.pretty_name
         @devices[i] = d.open
-        @threads << Thread.new do
-          mainloop @devices[i]
-        end
       end
 
-      @threads.each { |t| t.join } unless @threads.nil?
+      mainloop unless @devices.empty?
     end
 
-    def mainloop(device)
+    def mainloop
       loop do
-        device.gets.each do |data|
-          timestamp = sprintf "%12.4f", data[:timestamp]
-          ms = @ni.parse(*data[:data])
-          if ms.is_a? Array
-            ms.each do |mss|
-              puts "#{timestamp} #{device.name} #{Midicat::Formatter::format mss}"
-            end
-          else
-            puts "#{timestamp} #{device.name} #{Midicat::Formatter::format ms}"
-          end
+        @devices.each do |device|
+          device.gets.each do |data|
+            timestamp = sprintf "%12.4f", data[:timestamp]
 
+            ms = @ni.parse(*data[:data])
+            if ms.is_a? Array
+              ms.each do |mss|
+                puts "#{timestamp} #{device.name} #{Midicat::Formatter::format mss}"
+              end
+            else
+              puts "#{timestamp} #{device.name} #{Midicat::Formatter::format ms}"
+            end
+
+          end
         end
       end
     end
